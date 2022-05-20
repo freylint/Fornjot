5d727030f1637fc9e82242f370bfd06ed4aa5abf
Lucas Priestley <lmpriestley@tuta.io>
2022-05-20 14:47:41 -0500

    Create wasm based example model.

---
+++ models/wasm_cube/README.md
@@ -0,0 +1,10 @@
+# Fornjot - WASM Cube
+
+A model of a simple cuboid that demonstrates sweeping a 3D shape from a primitive with multiple connected edges on the wasm platform.
+
+To display this model, run the following from the repository root (model parameters are optional):
+``` sh
+cargo run -- --model wasm_cube --parameters x=3.0,y=2.0,z=1.0
+```
+
+![Screenshot of the cuboid model](cuboid.png)
---
+++ models/wasm_cube/src/lib.rs
@@ -0,0 +1,24 @@
+#![doc = include_str!("../README.md")]
+
+use std::collections::HashMap;
+use wasm_bindgen::prelude::*;
+
+#[no_mangle]
+#[wasm_bindgen]
+pub fn model() -> fj::Shape {
+    let x: f64 = 3.0;
+    let y: f64 = 2.0;
+    let z: f64 = 1.0;
+
+    #[rustfmt::skip]
+    let rectangle = fj::Sketch::from_points(vec![
+        [-x / 2., -y / 2.],
+        [ x / 2., -y / 2.],
+        [ x / 2.,  y / 2.],
+        [-x / 2.,  y / 2.],
+    ]).with_color([100,255,0,200]);
+
+    let cuboid = fj::Sweep::from_path(rectangle.into(), [0., 0., z]);
+
+    cuboid.into()
+}
